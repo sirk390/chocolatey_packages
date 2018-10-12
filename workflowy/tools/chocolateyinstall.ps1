2ce7efd785242efaef511fd921e40ee8db660c50
Christian BOdt <christian.bodt@coinplus.com>
2018-10-12 09:31:56 +0200

    -Update workflowy

--- workflowy/tools/chocolateyinstall.ps1
+++ workflowy/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/workflowy/desktop/releases/download/v1.0.15/WorkFlowy-Installer.exe'
+$url        = 'https://github.com/workflowy/desktop/releases/download/v1.1.4/WorkFlowy-Installer.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
 
   softwareName  = 'workflowy*'
 
-  checksum      = 'CCCF211CB38CAA7A48FC40308C8162ABFE0FE1F019D3BEB00DA03BC68CE370AF'
+  checksum      = '7C132A8CE6C7EA5EA8FA5FE1723B30A1B3E164683F6CBC844279B4D01519A48D'
   checksumType  = 'sha256'
 
   validExitCodes= @(0, 3010, 1641)
